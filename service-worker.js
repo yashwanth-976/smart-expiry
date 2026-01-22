d6ec4ffc9e4ec6ee6063b36699c4fcc5eec9df27
yashwanth-976 <yashwanthkoppada@gmail.com>
2026-01-23 00:22:27 +0530

    Update service worker cache version to v6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v5"; // ⬅️ CHANGE THIS EVERY DEPLOY
+const CACHE_VERSION = "v6"; // ⬅️ CHANGE THIS EVERY DEPLOY
 const CACHE_NAME = `smart-expiry-${CACHE_VERSION}`;
 
 const ASSETS = [
@@ -41,5 +41,6 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
